e51177e3095222425c719f78ee15edfc80cb97c9
kurnia123 <rizkykurnia219@gmail.com>
2021-11-19 12:43:07 +0700

    update path API

--- app/src/main/java/com/example/myapplication/data/remote/ApiInterface.java
+++ app/src/main/java/com/example/myapplication/data/remote/ApiInterface.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
-    @GET("api/v1/json/1/search_all_teams.php")
+    @GET("api/v1/json/2/search_all_teams.php")
     Call<ResultTeam> getAllTeam(
             @Query("s") String s,
             @Query("c") String c
